467fa1e917121d2db0c8b606b702595cd48f90ff
nexceed-dev <d304219@edu.curio.nl>
2026-02-12 13:47:26 +0100

    fix: display user role in the sidebar instead of static 'Medewerker'

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -83,7 +83,7 @@
                     </div>
                     <div class="flex-1">
                         <div class="font-semibold text-gray-800 text-sm">{{ auth()->user()->name }}</div>
-                        <div class="text-xs text-gray-500">Medewerker</div>
+                        <div class="text-xs text-gray-500">{{ Str::ucfirst(auth()->user()->role) }}</div>
                     </div>
                 </a>
                 <form method="POST" action="{{ route('logout') }}">
